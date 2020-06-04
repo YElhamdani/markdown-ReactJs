3895d8dd0ddb11b6f331e5be8ad596a48522d520
Younes <48552550+YElhamdani@users.noreply.github.com>
2020-06-04 17:54:02 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6">
+          <textarea
+          className="form-control"
+          rows="35"
+          ></textarea>
+        </div>
+
+        <div className="col-sm-6">
+          <h1>resultat</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
