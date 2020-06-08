8883aed511b6d1907589b0142da0da35881579fe
Younes <48552550+YElhamdani@users.noreply.github.com>
2020-06-08 21:10:04 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import { sampleText } from './sampleText'
+
+class App extends Component{
+  state={
+    text: sampleText
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState = ({text})
+  }
+  
+  render(){
+  return (
+    <div className="container">
+      <div className="row">
+      <div className='col-sm-6'>
+            <textarea
+              onChange={this.handleChange}
+              value={this.state.text}
+              className='form-control'
+              rows='35' />
+          </div>
+
+        <div className="col-sm-6">
+          <div>
+            {sampleText}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+}
+
+export default App;
