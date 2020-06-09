157c56bd05c12b8e674531dc4319f00c8c656f42
Younes <48552550+YElhamdani@users.noreply.github.com>
2020-06-09 22:51:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+import { sampleText } from './sampleText'
+
+class App extends Component{
+  state = {
+    text: sampleText
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+  
+  render(){
   return (
     <div className="container">
       <div className="row">
-        <div className="col-sm-6">
-          <textarea
-          className="form-control"
-          rows="35"
-          ></textarea>
-        </div>
+      <div className='col-sm-6'>
+            <textarea
+             onChange={this.handleChange}
+             value={this.state.text}
+              className='form-control'
+              rows='35' />
+          </div>
 
         <div className="col-sm-6">
-          <h1>resultat</h1>
+          <div>
+            {sampleText}
+          </div>
         </div>
       </div>
     </div>
   );
+}
 }
 
 export default App;
